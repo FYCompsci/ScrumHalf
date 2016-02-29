2be2caa2058ff1b12e739d95d74de71245f3fa8c
malsf21 <matthewang.wang@gmail.com>
2016-02-29 09:27:31 -0500

    adds skeleton for game

--- js/game.js
+++ js/game.js
@@ -1,1 +1,31 @@
 //template
+var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+  window.setTimeout(callback, 1000 / 60);
+};
+var canvas = $("#game-canvas")[0];
+var context = canvas.getContext('2d');
+var width = 800;
+var height = 800;
+
+var keysDown = {};
+
+var render = function () {
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, width, height);
+};
+
+var step = function () {
+    //update();
+    render();
+    animate(step);
+};
+
+animate(step);
+
+window.addEventListener("keydown", function (event) {
+    keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function (event) {
+    delete keysDown[event.keyCode];
+});
