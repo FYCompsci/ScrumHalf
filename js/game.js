efab0c844b167408286474485c9ade05bb760c07
malsf21 <matthewang.wang@gmail.com>
2016-02-29 19:09:51 -0500

    implements map render function

--- js/game.js
+++ js/game.js
@@ -9,6 +9,44 @@ var height = 600;
 
 var keysDown = {};
 
+function rect(x,y,w,h) {
+  context.beginPath();
+  context.rect(x,y,w,h);
+  context.closePath();
+  context.fill();
+  context.stroke();
+}
+function renderMap(map){
+  for (i = 0; i < map.length; i++){
+    console.log((i%25)*50);
+    console.log((i/25)*50);
+    if (map[i] == 1){
+      context.fillStyle = "green";
+      context.strokeStyle = "black";
+      rect((i%25)*50,(Math.floor(i/25))*50,50,50);
+    }
+    else{
+      context.fillStyle = "white";
+      context.strokeStyle = "black";
+      rect((i%25)*50,(Math.floor(i/25)*50),50,50);
+    }
+  }
+}
+var levelMap = [
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+];
+
 var update = function(){
     // template update function
 }
@@ -16,6 +54,7 @@ var update = function(){
 var render = function () {
     context.fillStyle = "#000000";
     context.fillRect(0, 0, width, height);
+    renderMap(levelMap);
 };
 
 var step = function () {
