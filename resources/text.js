51aa24a506d23312fe97a6cb2fd90d665a9fc980
malsf21 <matthewang.wang@gmail.com>
2016-03-07 22:08:28 -0800

    adds faux maps for level 2

--- resources/text.js
+++ resources/text.js
@@ -25,7 +25,36 @@ var world1_text = [
       "This looks pretty tough. Don't worry, I believe in you!", 400, 100
     ],
     [//stage 9
-      "Swag", 500, 500
+      "Wow, seems like the last puzzle piece. I wonder what happens if you collect it?", 400, 100
+    ]
+  ],
+  [//level 2
+    [//stage 1
+      "Ooh, a puzzle piece! Press the right arrow key to go get it!", 400, 100
+    ],
+    [//stage 2
+      "Oh no! A gap! Press the up arrow key to jump over it.", 400, 100
+    ],
+    [//stage 3
+      "Some platforms? Wonder if you can jump over them?", 400, 100
+    ],
+    [//stage 4
+      "Oh cool, a ladder! Just don't walk under it!", 400, 100
+    ],
+    [//stage 5
+      "Oh wow, that platform might need some repairs. Jump in the meantime.", 400, 100
+    ],
+    [//stage 6
+      "Hardcore Parkour? Seems dangerous.", 400, 100
+    ],
+    [//stage 7
+      "Careful, diving headfirst is the number 8 cause of concussions in WonderWorld!", 400, 100
+    ],
+    [//stage 8
+      "This looks pretty tough. Don't worry, I believe in you!", 400, 100
+    ],
+    [//stage 9
+      "Wow, seems like the last puzzle piece. I wonder what happens if you collect it?", 400, 100
     ]
   ]
 ];
