6e63a7136b79b0bc4202c6d61bace41ab18e8460
malsf21 <matthewang.wang@gmail.com>
2016-04-01 12:24:09 -0400

    adds a few more maps

--- resources/text.js
+++ resources/text.js
@@ -9,26 +9,26 @@ var world1_text = [
       ["Then, pick up that second puzzle piece. Pretty exciting!", 625, 100]
     ],
     [//stage 3
-      ["Some platforms? Wonder if you can jump over them?", 625, 100]
+      ["Some platforms? Wonder if you can jump over them?", 625, 75]
     ],
     [//stage 4
-      ["Oh cool, a ladder! Just don't walk under it!", 625, 100]
+      ["Oh cool, a ladder! Just don't walk under it!", 625, 75]
     ],
     [//stage 5
-      ["Oh snap, that platform might need some repairs. Jump in the meantime.", 625, 100]
+      ["Oh snap, that platform might need some repairs. Jump in the meantime.", 625, 75]
     ],
     [//stage 6
-      ["Hardcore Parkour? Seems dangerous.", 625, 100]
+      ["Hardcore Parkour? Seems dangerous.", 625, 75]
     ],
     [//stage 7
       ["Oh no, a fire? It's all good, you can jump over it!", 625, 75],
       ["That fire is probably Matt Wang's mixtape.", 625, 100]
     ],
     [//stage 8
-      ["This looks pretty tough. Don't worry, I believe in you!", 625, 100]
+      ["This looks pretty tough. Don't worry, I believe in you!", 625, 75]
     ],
     [//stage 9
-      ["Wow, seems like the last puzzle piece. I wonder what happens if you collect it?", 625, 100]
+      ["Wow, seems like the last puzzle piece. I wonder what happens if you collect it?", 625, 75]
     ]
   ],
   [//level 2
@@ -45,22 +45,22 @@ var world1_text = [
       ["Why do these enemies look so similar to you?", 625, 100]
     ],
     [//stage 4
-      ["That doesn't seem fun. Dropping the bass is so much better!", 625, 100]
+      ["That doesn't seem fun. Dropping the bass is so much better!", 625, 75]
     ],
     [//stage 5
-      ["Oh wow, that platform might need some repairs. Jump in the meantime.", 625, 100]
+      ["These setps aren't too easy to traverse. Too many enemies!", 625, 75]
     ],
     [//stage 6
-      ["Hardcore Parkour? Seems dangerous.", 625, 100]
+      ["More Hardcore Parkour? Seems super dangerous!", 625, 75]
     ],
     [//stage 7
-      ["Careful, diving headfirst is the number 8 cause of concussions in WonderWorld!", 625, 100]
+      ["Careful, diving headfirst is the number 8 cause of concussions in WonderWorld!", 625, 75]
     ],
     [//stage 8
-      ["This looks pretty tough. Don't worry, I believe in you!", 625, 100]
+      ["This looks pretty tough. Don't worry, I believe in you!", 625, 75]
     ],
     [//stage 9
-      ["Wow, seems like the last puzzle piece. I wonder what happens if you collect it?", 625, 100]
+      ["Wow, seems like the last puzzle piece. I wonder what happens if you collect it?", 625, 75]
     ]
   ]
 ];
